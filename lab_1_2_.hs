332eb2e82611ec05f1c43c52faa740770fde1af3
germanganys <german.ganys@gmail.com>
2022-11-21 13:42:57 +0300

    lab1

--- lab_1_2_.hs
+++ lab_1_2_.hs
@@ -1,5 +1,9 @@
 fibonacci = 1 : 2 : (zipWith (+) fibonacci (tail fibonacci))
 
-main = putStrLn $ show $ 
-       sum $ filter (\x -> x `mod` 2 == 0) $ 
-       takeWhile (< 4000000) $ fibonacci 
+main =
+    putStrLn $
+        show $
+            sum $
+                filter (\x -> x `mod` 2 == 0) $
+                    takeWhile (< 4000000) $
+                        fibonacci
